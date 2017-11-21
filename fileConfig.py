783ffa54a7621ce17583fa826a1875466ee2d808
edsonw <969693577@qq.com>
2017-11-21 15:43:39 +0800

    更新转换金额文件路径

--- fileConfig.py
+++ fileConfig.py
@@ -2,9 +2,14 @@
 
 class fileConfig:
 
-    orderFile = unicode(r'D:\京东金融\t_order.csv', 'utf-8')
-    tmp_orderFile = unicode(r'D:\京东金融\t_order1.csv', 'utf-8')
+    orderFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\t_order.csv', 'utf-8')
+    tmp_orderFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\t_order1.csv', 'utf-8')
+    userFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\t_user.csv', 'utf-8')
+    loanSumFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\t_loan_sum.csv', 'utf-8')
+    clickFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\t_click.csv', 'utf-8')
+    loanFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\t_loan.csv', 'utf-8')
 
-    userFile = unicode(r'D:\京东金融\t_user.csv', 'utf-8')
-    loanSumFile = unicode(r'D:\京东金融\t_loan_sum.csv', 'utf-8')
-    clickFile = unicode(r'D:\京东金融\t_click.csv', 'utf-8')
+    CovertedorderFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\converted_t_order.csv', 'utf-8')
+    CovertedloanFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\converted_t_loan.csv', 'utf-8')
+    CovertedloanSumFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\converted_t_loan_sum.csv', 'utf-8')
+    CoverteduserFile = unicode(r'E:\PyCharm Project\JDD_Loan_Forecasting_Qualification_2017\input\Loan_Forecasting_Qualification\converted_t_user.csv', 'utf-8')
