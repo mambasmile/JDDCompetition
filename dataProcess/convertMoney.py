5f64fce74657153ca52fe79680d7524aecde1ce8
edsonw <969693577@qq.com>
2017-11-21 15:44:00 +0800

    将所有金额转为实际金额

---
+++ dataProcess/convertMoney.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/21 14:50
+# @Author  : sunday
+# @Site    : 
+# @File    : convertMoney.py
+# @Software: PyCharm
+from datetime import datetime
+import pandas as pd
+import json
+import numpy as np
+from fileConfig import fileConfig
+"""将文件中所有的金额数据转换为实际数据"""
+class convertMoney:
+    def convertMoney(self, money):
+        try:
+            money = np.power(5,money)-1
+            return money
+        except:
+            print money
+
+    def save2File(self, data, path):
+        data.to_csv(path,index = False)
+
+
+
+    def run(self):
+        orderFile = pd.read_csv(fileConfig.orderFile)
+        loanFile = pd.read_csv(fileConfig.loanFile)
+        loanSumFile = pd.read_csv(fileConfig.loanSumFile)
+        userFile = pd.read_csv(fileConfig.userFile)
+
+        # print self.convertOrder(orderFile.price)
+        orderFile.discount = self.convertMoney(orderFile.discount)
+        orderFile.price = self.convertMoney(orderFile.price)
+        userFile.limit = self.convertMoney(userFile.limit)
+
+        loanFile.loan_amount = self.convertMoney(loanFile.loan_amount)
+        loanSumFile.loan_sum = self.convertMoney(loanSumFile.loan_sum)
+
+        self.save2File(loanFile, fileConfig.CovertedloanFile)
+        self.save2File(loanSumFile, fileConfig.CovertedloanSumFile)
+        self.save2File(orderFile, fileConfig.CovertedorderFile)
+        self.save2File(userFile, fileConfig.CoverteduserFile)
+
+convert = convertMoney()
+convert.run()
