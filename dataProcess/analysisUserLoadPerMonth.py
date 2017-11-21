4d0147321579c33988962d75642a64122fd30f25
mambasmile <13277061183@163.com>
2017-11-21 19:15:48 +0800

    更新语料库

---
+++ dataProcess/analysisUserLoadPerMonth.py
@@ -0,0 +1,56 @@
+#coding=utf-8
+
+import pandas as pd
+from fileConfig import fileConfig
+from datetime import datetime
+
+def analysisUserLoadPerMonth(trainCorpus=True):
+    userDF = pd.read_csv(fileConfig.userFile)
+    # with open()
+    if trainCorpus:
+        monthLs = [8,9,10]
+    else:
+        monthLs = [9,10,11]
+    firstMonthLoad = []
+    secondMonthLoad = []
+    thirdMonthLoad = []
+    uidLs = []
+
+    userLoadDF = pd.read_csv(fileConfig.timeCovertedloanFile)
+    for name,group in userLoadDF.groupby('uid'):
+        uidLs.append(name)
+        tmpSet = set()
+        for subname,subgroup in group.groupby('loan_time'):
+            tmpSet.add(subname)
+            if subname == monthLs[0]:
+                firstMonthLoad.append(sum(subgroup.loan_amount.tolist()))
+            elif subname == monthLs[1]:
+                secondMonthLoad.append(sum(subgroup.loan_amount.tolist()))
+            elif subname == monthLs[2]:
+                thirdMonthLoad.append(sum(subgroup.loan_amount.tolist()))
+            # else:
+            #     print subgroup
+        resLs = list(set(monthLs) - tmpSet)
+        for ele in resLs:
+            if ele == monthLs[0]:
+                firstMonthLoad.append(0.0)
+            elif ele == monthLs[1]:
+                secondMonthLoad.append(0.0)
+            elif ele == monthLs[2]:
+                thirdMonthLoad.append(0.0)
+
+    resDF = pd.DataFrame({'uid':uidLs,
+                          'firstMonthLoad':firstMonthLoad,
+                          'secondMonthLoad':secondMonthLoad,
+                          'thirdMonthLoad':thirdMonthLoad,
+                          })
+    resDF = pd.concat([resDF,userDF[['uid']]],axis=1)
+    # resDF.uid = resDF.uid.astype(int)
+    resDF = resDF.fillna(0.0)
+    if trainCorpus:
+        resDF.to_csv('../secondCorpus/train_userMonthLoad.csv',index=False)
+    else:
+        resDF.to_csv('../secondCorpus/test_userMonthLoad.csv', index=False)
+
+if __name__ == '__main__':
+    analysisUserLoadPerMonth(trainCorpus=True)
---
+++ dataProcess/concertTime.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+
+from datetime import datetime
+from fileConfig import fileConfig
+
+"""取时间里面的月"""
+class convertTime:
+
+    @staticmethod
+    def extractMonth(symbol,filepath,outpath):
+        i = 0
+        with open(outpath,'w') as e:
+            with open(filepath,'r') as e1:
+                for line in e1:
+                    if i == 0:
+                        i+=1
+                        e.write(line)
+                        continue
+                    data = line.strip().split(',')
+                    if symbol == 1:
+                        month = datetime.strptime(data[1],'%Y-%m-%d %H:%M:%S').month
+                    elif symbol == 0:
+                        month = datetime.strptime(data[1],'%Y-%m-%d').month
+                    data[1] = str(month)
+                    e.write(",".join(data)+'\n')
+
+if __name__ == '__main__':
+    convertTime.extractMonth(1,fileConfig.CovertedloanFile,fileConfig.timeCovertedloanFile)
